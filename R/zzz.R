d66b811eeb681f718ca0a957092163fa096c8132
nzkermani <nz1413@ic.ac.uk>
2025-04-30 13:32:45 +0100

    Update zzz.R to auto-load harmonized_database_dynamic

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,9 @@
 .onLoad <- function(libname, pkgname) {
-  # First, try loading the backup if it exists
-  if (file.exists("backup/harmonized_database_dynamic_backup.rds")) {
-    harmonized_database_dynamic <<- readRDS("backup/harmonized_database_dynamic_backup.rds")
-    packageStartupMessage("Loaded dynamic harmonization database from backup.")
-  } else {
-    # If not, initialize a fresh empty database
-    harmonized_database_dynamic <<- tibble::tibble(
-      `Harmonized Name` = character(),
-      `UK Variable` = character(),
-      `Source Label / Description` = character(),
-      `Target Concept` = character(),
-      `Standard Vocabulary` = character(),
-      `Standard Concept Code` = character(),
-      `Match Confidence` = character(),
-      `Summary` = character(),
-      `Timestamp Added` = as.POSIXct(character())
-    )
-    packageStartupMessage("No backup found. Initialized empty harmonized_database_dynamic.")
-  }
+  # First, try loading the  if it exists
+
+
+    harmonized_database <<- load("data/harmonized_database.rda")
+    harmonized_database_dynamic <<- harmonized_database
+
+
 }
