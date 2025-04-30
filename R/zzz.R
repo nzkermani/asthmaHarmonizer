9dbf7868f851eb483dfc888c925d3369caf4f1bc
Nazanin z. Kermani <nz1413@ic.ac.uk>
2025-04-30 13:54:39 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,16 @@
   # First, try loading the  if it exists
 
 
-    harmonized_database <<- load("data/harmonized_database.rda")
-    harmonized_database_dynamic <<- harmonized_database
+    if (file.exists("backup/harmonized_database_dynamic_backup.rds")) {
+      harmonized_database_dynamic <<- readRDS("backup/harmonized_database_dynamic_backup.rds")
+      packageStartupMessage("Loaded dynamic harmonization database from backup.")
 
+    } else {
+      harmonized_database_dynamic <<- readRDS("data/harmonized_database_dynamic_backup.rds")
+
+      packageStartupMessage("No backup found. Initialized empty harmonized_database_dynamic.")
+
+
+    }
 
 }
